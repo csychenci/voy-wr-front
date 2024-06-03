b00980c8af0dc2bc89b80bdd1b1cfd24717a4905
wuzhanglun <chencicsy@gmail.com>
2024-06-03 18:54:07 +0800

    update

---
+++ scripts/prepare.sh
@@ -0,0 +1,11 @@
+echo "Load current nvm dir..."
+CURRENT_NVM_DIR="${NVM_DIR}"
+echo "Current nvm dir: ${CURRENT_NVM_DIR}, load nvm..."
+[ -s "$CURRENT_NVM_DIR/nvm.sh" ] && \. "$CURRENT_NVM_DIR/nvm.sh"  # 加载 nvm
+# if [ -z "$NVM_DIR" ]; then
+#   echo "nvm not found"
+# else
+#   nvm use 16.15.1
+# fi
+nvm use 18.19.1
+npm install
