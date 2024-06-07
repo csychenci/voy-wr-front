d0806d770e98b679c87e4c3398e86601eca4859b
wuzhanglun <chencicsy@gmail.com>
2024-06-07 14:56:49 +0800

    feat: delete other files

--- src/components/map/index.tsx
+++ src/components/map/index.tsx
@@ -6,14 +6,14 @@ import React from "react";
 
 console.log("style", style);
 
-const Map = forwardRef<{}, MapProps>((props, ref) => {
+const Map = forwardRef<{}, MapProps>(({ className }, ref) => {
 	const mapContainer = useRef<HTMLDivElement>(null);
 	const mapbox = useRef<mapboxgl.Map>(null);
 	const init = () => {
 		mapbox.current = new mapboxgl.Map({
 			container: mapContainer.current,
-			// style: "mapbox://styles/mapbox/streets-v11",
-			style: "mapbox://styles/voyagecentury2018/cln1634sd003g01rc1alr5qmp",
+			style: "mapbox://styles/mapbox/streets-v11",
+			// style: "mapbox://styles/voyagecentury2018/cln1634sd003g01rc1alr5qmp",
 			accessToken:
 				"pk.eyJ1Ijoidm95YWdlY2VudHVyeTIwMTgiLCJhIjoiY2xmOTM0NDJyMXV1MjQwb2NrNzFvZWJ3dSJ9.zyBT0Gd6HYyeywftD_XKtg",
 			center: { lng: 174.7265625, lat: 29.305561325527698 },
--- src/components/map/type.ts
+++ src/components/map/type.ts
@@ -1,1 +1,3 @@
-export type MapProps = {};
+export type MapProps = {
+	className?: string;
+};
